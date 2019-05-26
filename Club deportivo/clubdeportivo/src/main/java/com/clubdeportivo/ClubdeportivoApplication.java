ded311c0cb54107dd489a094f3f6e64ea2cffd14
roroxs16 <38821332+roroxs16@users.noreply.github.com>
2019-05-26 14:33:20 -0400

    Estructura de las clases modelos
    Se crea la estructura de las clase modelos en Spring tool Suite

---
+++ Club deportivo/clubdeportivo/src/main/java/com/clubdeportivo/ClubdeportivoApplication.java
@@ -0,0 +1,13 @@
+package com.clubdeportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubdeportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubdeportivoApplication.class, args);
+	}
+
+}
