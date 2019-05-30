91bd04a9947361c06033d04fc54f5e5afb4f5739
roroxs16 <38821332+roroxs16@users.noreply.github.com>
2019-05-30 11:08:06 -0400

    [Update]
    -SocioCategoriaDao.java: se actualiza uno de los imports

--- clubdeportivo2/src/main/java/com/clubdeportivo/repository/SocioCategoriaDao.java
+++ clubdeportivo2/src/main/java/com/clubdeportivo/repository/SocioCategoriaDao.java
@@ -1,6 +1,7 @@
 package com.clubdeportivo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
 import com.clubdeportivo.model.SocioCategoriaModel;
