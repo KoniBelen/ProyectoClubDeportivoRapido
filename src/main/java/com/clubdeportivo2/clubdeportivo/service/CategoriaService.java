9afcfff50dc4dae2d5a93682ebc8d86d7fcc946b
XanderSapiens <alexander.cuevas1501@alumnos.ubiobio.cl>
2019-06-03 17:16:40 -0400

    [ADDED] CategoriaService
    -Se agrega la clase categoriaService

---
+++ src/main/java/com/clubdeportivo2/clubdeportivo/service/CategoriaService.java
@@ -0,0 +1,12 @@
+package com.clubdeportivo2.clubdeportivo.service;
+
+import com.clubdeportivo2.clubdeportivo.commons.GenericService;
+import com.clubdeportivo2.clubdeportivo.model.CategoriaModel;
+
+
+
+public interface CategoriaService extends GenericService<CategoriaModel, Integer>
+
+{
+
+}
