55a1e62a510a4c74457e240e28f7db63803bd9cb
JuanMEMOUBB <43252326+JuanMEMOUBB@users.noreply.github.com>
2019-06-19 11:52:37 -0400

    Revert "[update]"
    This reverts commit dd7c0f48c603e47b7c9b474fc3715a92c2a3ad8a.

--- src/main/java/com/clubdeportivo2/clubdeportivo/dao/EquipoDao.java
+++
@@ -1,9 +0,0 @@
-package com.clubdeportivo2.clubdeportivo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.clubdeportivo2.clubdeportivo.model.EquipoModel;
-
-public interface EquipoDao extends CrudRepository<EquipoModel, Integer> {
-
-}
--- src/main/java/com/clubdeportivo2/clubdeportivo/service/EquipoService.java
+++
@@ -1,9 +0,0 @@
-package com.clubdeportivo2.clubdeportivo.service;
-
-import com.clubdeportivo2.clubdeportivo.commons.GenericService;
-import com.clubdeportivo2.clubdeportivo.model.EquipoModel;
-
-public interface EquipoService extends GenericService<EquipoModel, Integer> {
-
-
-}
