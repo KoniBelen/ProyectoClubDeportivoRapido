dd7c0f48c603e47b7c9b474fc3715a92c2a3ad8a
JuanMEMOUBB <43252326+JuanMEMOUBB@users.noreply.github.com>
2019-06-19 11:40:54 -0400

    [update]
    creación clases Equipo

---
+++ src/main/java/com/clubdeportivo2/clubdeportivo/dao/EquipoDao.java
@@ -0,0 +1,9 @@
+package com.clubdeportivo2.clubdeportivo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.clubdeportivo2.clubdeportivo.model.EquipoModel;
+
+public interface EquipoDao extends CrudRepository<EquipoModel, Integer> {
+
+}
