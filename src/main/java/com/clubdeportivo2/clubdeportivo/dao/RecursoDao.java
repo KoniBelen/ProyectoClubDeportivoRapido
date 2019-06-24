9b4f1a8d8840ab65ba3f5838e23a1fc054236de6
XanderSapiens <44037786+XanderSapiens@users.noreply.github.com>
2019-06-24 19:14:29 -0400

    [ADD] Todo lo relativo a recursos
    -RecursoModel
    -RecursoDao
    -RecursoService
    -RecursoServiceImpl
    -RecursoController
    -Update cuotaController y navbar

---
+++ src/main/java/com/clubdeportivo2/clubdeportivo/dao/RecursoDao.java
@@ -0,0 +1,14 @@
+package com.clubdeportivo2.clubdeportivo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.clubdeportivo2.clubdeportivo.model.RecursoModel;
+import com.clubdeportivo2.clubdeportivo.model.EquipoModel;
+
+public interface RecursoDao extends CrudRepository<RecursoModel, Integer> {
+
+}
