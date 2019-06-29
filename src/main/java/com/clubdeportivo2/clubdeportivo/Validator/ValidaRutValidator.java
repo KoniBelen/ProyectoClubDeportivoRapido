97655665094e36c433349f6d80155b1f0c8a2dac
KoniBelen <konibelen12@gmail.com>
2019-06-28 20:09:12 -0400

    [ADD] Se agrega el Custom Validator para el RutSocio

---
+++ src/main/java/com/clubdeportivo2/clubdeportivo/Validator/ValidaRutValidator.java
@@ -0,0 +1,36 @@
+package com.clubdeportivo2.clubdeportivo.Validator;
+
+import static java.lang.Character.getNumericValue;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ValidaRutValidator implements ConstraintValidator<ValidaRut, String>{
+
+	@Override
+	public boolean isValid(String rut,ConstraintValidatorContext context) {
+		if(!rut.matches("\\d{3,8}-[\\d|kK]{1}") )    return false;
+		return rutAlgorithm(rut);
+	}
+	
+	private boolean rutAlgorithm(String rut) {
+		String [] rutDividido = rut.split("-");
+		System.out.println(rutDividido[0]);
+        char[] rutSinDigito =rutDividido[0].toCharArray();
+        int factor=0;
+        int suma =0;
+        int largoRutSinDigito=rutSinDigito.length;
+        for (int i = largoRutSinDigito-1; i >= 0 ; i--,factor++) {
+        	suma+=getNumericValue(rutSinDigito[i]) * (factor%6 +2);
+        		
+        }
+        String digVer = 11-suma%11+"";
+        if(digVer.equals("11")) 
+        	digVer="0";
+        else if(digVer.equals("10"))
+        	digVer="k";
+        System.out.println("suma: "+suma +"\tDigVerificador: "+digVer);
+        return digVer.equalsIgnoreCase(rutDividido[1]);  		
+	}
+
+}
